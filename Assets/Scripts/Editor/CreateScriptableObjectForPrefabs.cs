d653cd0b4124e4d8fc61f9791a72c84cc4186eee
Akrima Huzaifa Akhtar <akrimaamirka@gmail.com>
2023-05-24 19:08:21 +0500

    Change some Architecture(wip)
    Automate Scriptable Objects
    Naming Images are remaining

---
+++ Assets/Scripts/Editor/CreateScriptableObjectForPrefabs.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateScriptableObjectForPrefabs : EditorWindow
+{
+    private string prefabFolderPath = "Assets/Prefabs/Perfabs-Models/Furnitures/Bed/";
+    private string scriptableObjectFolderPath = "Assets/AddressableAssets/Items/";
+    private bool searchInSubDirectories;
+
+    [MenuItem("Window/Create ScriptableObjects for Prefabs")]
+    private static void ShowWindow()
+    {
+        GetWindow<CreateScriptableObjectForPrefabs>("Create ScriptableObjects");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Create ScriptableObjects for Prefabs\n" +
+                        "_____________________________________", EditorStyles.boldLabel);
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Get Prefab from Folder => Path:", EditorStyles.boldLabel);
+        prefabFolderPath = EditorGUILayout.TextField(prefabFolderPath);
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Create ScriptableObject in Folder => Path:", EditorStyles.boldLabel);
+        scriptableObjectFolderPath = EditorGUILayout.TextField(scriptableObjectFolderPath);
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Search in Sub-Directories By Parent Directory: ", EditorStyles.boldLabel);
+        searchInSubDirectories = EditorGUILayout.Toggle(searchInSubDirectories);
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Create ScriptableObjects"))
+        {
+            CreateScriptableObjects();
+        }
+    }
+
+    private void CreateScriptableObjects()
+    {
+        if (searchInSubDirectories)
+        {
+            string[] prefabPaths = Directory.GetFiles(prefabFolderPath, "*.prefab", SearchOption.AllDirectories);
+            foreach (string prefabPath in prefabPaths)
+            {
+                CreateAssetAtPath(prefabPath);
+            }
+        }
+        else
+        {
+            // Get all the prefabs in the specified folder path
+            string[] prefabGuids = AssetDatabase.FindAssets("t:Prefab", new[] { prefabFolderPath });
+            foreach (string prefabGuid in prefabGuids)
+            {
+                string prefabPath = AssetDatabase.GUIDToAssetPath(prefabGuid);
+                CreateAssetAtPath(prefabPath);
+            }
+        }
+    }
+
+    private void CreateAssetAtPath(string prefabPath)
+    {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+
+        // Create a ScriptableObject with the same name as the prefab
+        string fileName = Path.GetFileName(prefabPath);
+        //string scriptableObjectPath = prefabPath.Replace(prefabFolderPath, scriptableObjectFolderPath);
+        string scriptableObjectPath = scriptableObjectFolderPath + fileName;
+        scriptableObjectPath = scriptableObjectPath.Replace(".prefab", ".asset");
+
+        if (!File.Exists(scriptableObjectPath))
+        {
+            Item item = ScriptableObject.CreateInstance<Item>();
+            AssetDatabase.CreateAsset(item, scriptableObjectPath);
+
+            // Save the changes
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            Item objToReset = AssetDatabase.LoadAssetAtPath<Item>(scriptableObjectPath);
+            objToReset.Reset();
+            Debug.Log("Created ScriptableObject for prefab: " + prefab.name);
+        }
+        else
+        {
+            Debug.Log("File Already Exist!");
+        }
+    }
+}
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,6 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.WSA;
 
 [CreateAssetMenu(fileName = "Item1", menuName = "Add Item/Item")]
 public class Item : ScriptableObject
@@ -8,4 +9,97 @@ public class Item : ScriptableObject
     public float price;
     public GameObject itemPrefab;
     public Sprite itemImage;
+
+    [SerializeField] private bool isFurniture = true;
+    [SerializeField] private bool searchInSubDirectories;
+    [SerializeField] private string objectName;
+    [SerializeField] private string prefabPath;
+    [SerializeField] private string texturePath;
+
+    #if UNITY_EDITOR
+    public void Reset()
+    {
+        price = 100;
+        SetPrefabPath();
+        texturePath = "Assets/Textures/Sprites";
+        GetObjectName();
+        GetItemPrefab();
+        SetItemImage();
+    }
+
+
+
+    private void SetPrefabPath()
+    {
+        if (isFurniture)
+        {
+            prefabPath = "Assets/Prefabs/Perfabs-Models/Furnitures/";
+        }
+        else
+        {
+            if (Directory.Exists("Assets/Prefabs/Perfabs-Models/WallObjects"))
+            {
+                prefabPath = "Assets/Prefabs/Perfabs-Models/WallObjects/";
+            }
+        }
+    }
+
+    private void GetObjectName()
+    {
+        // Get the asset file path of the ScriptableObject
+        string assetPath = AssetDatabase.GetAssetPath(this);
+
+        // Get the file name without extension
+        objectName = Path.GetFileNameWithoutExtension(assetPath);
+    }
+
+    private void GetItemPrefab()
+    {
+        if (isFurniture)
+        {
+            if (searchInSubDirectories)
+            {
+                //---getting all folders from Pole---
+                string[] allFolders = Directory.GetDirectories(prefabPath);
+                foreach (string folderPath in allFolders)
+                {
+                    //Debug.Log(folderPath);
+                    //Debug.Log(objectName);
+                    var folderName = Path.GetFileName(folderPath);
+                    if (objectName.Contains(folderName))
+                    {
+                        var files = Directory.GetFiles(folderPath, "*.prefab");
+                        foreach (string file in files)
+                        {
+                            //Debug.Log(file);
+                            //Debug.Log(objectName);
+                            if (file.Contains(objectName))
+                            {
+                                itemPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(file);
+                            }                               
+                        }
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    private void SetItemImage()
+    {
+        var files = Directory.GetFiles(texturePath, "*.png");
+        foreach(string file in files)
+        {
+            //Debug.Log(file);
+            //Debug.Log(objectName);
+            if (file.Contains(objectName))
+            {
+                itemImage = AssetDatabase.LoadAssetAtPath<Sprite>(file);
+            }
+        }
+    }
+    #endif
 }
